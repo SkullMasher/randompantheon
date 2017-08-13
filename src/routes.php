7a4b9a9d4b4c36e0f57a1f450cdf95e7fb58301c
Florian Ledru <skullm4sher@gmail.com>
2017-08-13 17:35:36 +0200

    :skull: added admin route with coresponding view

--- src/routes.php
+++ src/routes.php
@@ -1,10 +1,15 @@
 <?php
 // Routes
 
-$app->get('/[{name}]', function ($request, $response, $args) {
-    // Sample log message
-    $this->logger->info("Slim-Skeleton '/' route");
+$app->get('/', function ($request, $response, $args) {
+  // Sample log message
+  $this->logger->info(" '/' route");
 
-    // Render index view
-    return $this->view->render($response, 'index.twig', $args);
+  // Render index view
+  return $this->view->render($response, 'homepage.twig', $args);
+});
+
+$app->get('/admin', function ($request, $response, $args) {
+  $this->logger->info(" '/admin' route");
+  return $this->view->render($response, 'admin.twig', $args);
 });
